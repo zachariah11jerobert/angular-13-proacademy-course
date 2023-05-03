a010cff05d6be09319ae3670f645f3e27bb0990d
jerome <zachariah11jerobert@gmail.com>
2023-05-03 10:04:56 +0530

    initial repo

---
+++ angular-ex1-basics/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-ex1-basics';
+  message='Angular is the best JavaScript framework for building client-side application';
+}
